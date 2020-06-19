5f0d690ff076c435890fd5414c333d770f470e91
DK-So <54423001+DK-So@users.noreply.github.com>
2020-06-19 19:27:57 +0200

    Add files via upload

---
+++ dice_simulation/dice.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<random>
+#include<time.h>
+
+using namespace std;
+
+int main()
+{
+    // Variables 
+    int dicnmb = 0; //decision Number
+    int rndnmb; //random number
+
+    /* initialize random seed */ 
+
+    srand(time(NULL));
+
+    do
+    {
+        /* generate dice number */
+      rndnmb = rand() % 7;
+      cout << "\033[1;36m" << rndnmb <<  "\033[0m"<< endl;
+      cout << "Do you want to roll the dice again? 1/Yes 0/No" << endl;
+      cin >>  dicnmb;
+    }while (dicnmb == 1);
+      cout << "Fairwell!" << endl;
+    return 0;
+
+
+}
